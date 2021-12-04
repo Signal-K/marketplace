3194c7f5071bf004b4e48a4befa1332a1fd27d7e
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-12-04 12:18:22 +0000

    =Creating mutate method for creating FUSDVault (async inside use-fusd hook)

--- src/hooks/use-fusd.hook.js
+++ src/hooks/use-fusd.hook.js
@@ -1,6 +1,7 @@
 import { useEffect, useReducer } from 'react'
 import { defaultReducer } from '../reducer/defaultReducer'
-import { query } from '@onflow/fcl'
+import { mutate, query } from '@onflow/fcl'
+import { GET_FUSD_BALANCE } from '../flow/get-fusd-balance.script'
 
 export default function useFUSD(user) {
   const [state, dispatch] = useReducer(defaultReducer, {
@@ -18,18 +19,7 @@ export default function useFUSD(user) {
     dispatch({ type: 'PROCESSING' });
     try {
       let response = await query({
-        cadence: `
-        import FungibileToken from 0xFungibleToken
-        import FUSD from 0xFUSD
-
-        pub fun main(address: Address): UFix64? {
-          let account = getAccount(address)
-          if let vaultRef = account.getCapability(/public/fusdBalance).borrow<&FUSD.Vault>{FungibleToken.Balance}> {
-            return vaultRef.balance
-          }
-          return nil
-        }
-        `,
+        cadence: GET_FUSD_BALANCE,
         args: (arg, t) => [arg(user?.addr, t.Address)]
       });
       dispatch({ type: 'SUCCESS', payload: response }); // Display's the user's FUSD balance on the frontend (variable response)
@@ -39,8 +29,44 @@ export default function useFUSD(user) {
     }
   }
 
+  const createFUSDVault = async () => {
+    dispatch({ type: 'PROCESSING' });
+    try {
+      let transaction = await mutate({
+        cadence: `
+          import FungibleToken from 0xFungibleToken
+          import FUSD from 0xFUSD
+
+          transaction {
+            prepare(signer: AuthAccount) {
+              if(signer.borrow<&FUSD.Vault>(from: /storage/fusdVault) != nil) {
+                return
+              }
+
+              signer.save(<-FUSD.createEmptyVault(), to: /storage/fusdVault)
+
+              signer.link<&FUSD.Vault{FungibleToken.Receiver}>(
+                /public/fusdReceiver,
+                target: /storage/fusdVault
+              )
+
+              signer.link<&FUSD.Vault{FungibleToken.Balance}>(
+                /public/fusdBalance,
+                target: /storage/fusdVault
+              )
+            }
+          }
+        `
+      })
+    } catch (err) {
+      dispatch({ type: 'ERROR' })
+      console.log(err)
+    }
+  }
+
   return {
     ...state,
-    getFUSDBalance
+    createFUSDVault,
+    getFUSDBalance,
   }
 }
