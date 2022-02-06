664c97aa02d245900d257bbfda219ee362421b3d
Gizmotronn <droidology@allianceofdroids.org.au>
2022-02-07 08:30:37 +1100

    🌈🩸 -> #16 Phaser/Unity scaffolding for ship ai

---
+++ marketplace-games/space-invaders/public/script.js
@@ -0,0 +1,10 @@
+const config = {
+    width: 1400,
+    height: 750,
+    backgroundColor: "#FFFFF",
+    parent: "gameContainer",
+    scene: [GameScene],
+    physics: {
+        default: 'arcade'
+    }
+};
